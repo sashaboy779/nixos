6be10fe7c04886169445c4ea23f5f681e3633db2
sashaboy779 <sashaboychuk779@gmail.com>
2025-02-22 21:32:25 +0200

    move to everforest theme

--- modules/home-manager/alacritty.nix
+++ modules/home-manager/alacritty.nix
@@ -3,13 +3,19 @@
   programs.alacritty = {
     enable = true;
     settings = {
-      window.opacity = 0.8;
+      window = {
+        opacity = 0.8;
+        padding = {
+          x = 10;
+          y = 10;
+        };
+      };
       scrolling.multiplier = 5;
       font = { 
         size = 12;
       };
       general.import = [
-        "${pkgs.alacritty-theme}/gruvbox_dark.toml"
+        "${pkgs.alacritty-theme}/everforest_dark.toml"
       ];
     };
   };
